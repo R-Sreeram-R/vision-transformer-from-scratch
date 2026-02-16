017b4ddae297d5b74e488c47e3085d63b2ad9e2b
Sreeram Raghammudi <115561315+R-Sreeram-R@users.noreply.github.com>
2026-02-16 17:00:00 -0500

    trainer engine completed

--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -30,5 +30,59 @@ def train_step(model,dataloader,loss_fn,optimizer,device):
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
-def train(model, train_loader, test_loader, optimizer, loss_fn):
-    ...
+def test_step(model, dataloader, loss_fn,device):
+
+  model.eval() 
+
+  test_loss, test_acc = 0, 0
+
+
+  with torch.inference_mode():
+      
+      for batch, (X, y) in enumerate(dataloader):
+          
+          X, y = X.to(device), y.to(device)
+
+          test_pred_logits = model(X)
+
+          loss = loss_fn(test_pred_logits, y)
+          test_loss += loss.item()
+
+          test_pred_labels = test_pred_logits.argmax(dim=1)
+          test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+
+  test_loss = test_loss / len(dataloader)
+  test_acc = test_acc / len(dataloader)
+  return test_loss, test_acc
+
+
+def train(model, train_loader, test_loader, optimizer, loss_fn, epochs, device):
+
+    results = {"train_loss": [],
+      "train_acc": [],
+      "test_loss": [],
+      "test_acc": []
+    }
+
+    for epoch in tqdm(range(epochs)):
+
+        train_loss, train_acc = train_step(model=model, dataloader=train_loader, loss_fn=loss_fn, optimizer=optimizer, device = device)
+        
+        test_loss, test_acc = test_step(model=model,dataloader=test_loader,loss_fn=loss_fn,device=device)
+
+        print(
+          f"Epoch: {epoch+1} | "
+          f"train_loss: {train_loss:.4f} | "
+          f"train_acc: {train_acc:.4f} | "
+          f"test_loss: {test_loss:.4f} | "
+          f"test_acc: {test_acc:.4f}"
+        ) 
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    return results
+
+
